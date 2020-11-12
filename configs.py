e4a976dea371ecfa5d939c76d52c026795d63d99
VadyChel <zyzel19@gmail.com>
2020-11-12 13:24:08 +0200

    Untracked files issue resolved to fix .gitignore

--- configs.py
+++ configs.py
@@ -1,19 +1,20 @@
 import os
 
 configs = {
-	'DEF_PREFIX': '*', # On
+	'DEF_PREFIX': '*',
 	'OWNERS': [660110922865704980, 404224656598499348],
 	'SHOPLIST_COSTS': [500, 1000, 100, 1100, 100, 600],
 	'MIN_BALANCE': -5000,
-	'DEF_PROFILE_BG': './Data/Img/lime.png', # On
-	'FONT': './Data/Fonts/a_AlternaSw.ttf', # On
-	'SAVE_IMG': './Data/Img/usercard.png', # On
+	'TEMP_PATH': './Data/TempFiles/',
+	'DEF_PROFILE_BG': './Data/Img/lime.png',
+	'FONT': './Data/Fonts/a_AlternaSw.ttf',
+	'SAVE_IMG': './Data/Img/usercard.png',
 	'BOT_NAME': 'ProstoTools.exe',
 	'BOT_ID': 700767394154414142,
 	'HELP_SERVER': 'https://discord.gg/CXB32Mq',
-	'FOOTER_TEXT': 'ProstoTools.exe || Copyright 2020-2020', # On
-	'MUTE_ROLE': 'PT-MUTED', # On
-	'VMUTE_ROLE': 'PT-VMUTED', # On
-	'SOFTBAN_ROLE': 'PT-SOFT-BANNED', # On
-	'TOKEN': os.environ['BOT_TOKEN'], # On
+	'FOOTER_TEXT': 'ProstoTools.exe || Copyright 2020-2020',
+	'MUTE_ROLE': 'PT-MUTED',
+	'VMUTE_ROLE': 'PT-VMUTED',
+	'SOFTBAN_ROLE': 'PT-SOFT-BANNED',
+	'TOKEN': os.environ['BOT_TOKEN'],
 }
