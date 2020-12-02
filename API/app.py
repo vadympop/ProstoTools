bc141834feed30c6eef9a72d2f8672be82cb67c5
VadyChel <zyzel19@gmail.com>
2020-12-02 20:21:27 +0200

    Remove API to other repository

--- API/app.py
+++
@@ -1,9 +0,0 @@
-from core.api import bp
-from sanic import Sanic
-
-client = None
-app = Sanic(__name__)
-app.blueprint(bp)
-
-if __name__ == '__main__':
-	app.run(host="127.0.0.1", port=5000, debug=True)
