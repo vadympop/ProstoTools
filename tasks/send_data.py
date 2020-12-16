105ceb883b799c086583da63e9e5752a8792c92d
VadyChel <zyzel19@gmail.com>
2020-12-16 20:56:58 +0200

    Fix sending data to api

--- tasks/send_data.py
+++ tasks/send_data.py
@@ -11,34 +11,35 @@ class TasksSendData(commands.Cog):
 
 	@tasks.loop(hours=12)
 	async def send_data_loop(self):
-		data = {
-			"guilds": [guild.id for guild in self.client.guilds],
-			"users": [user.id for user in self.client.users],
-			"channels": [channel.id for channel in self.client.get_all_channels()],
-			"commands": {
-				cog_name: {
-					command.name: {
-						"description": command.description, 
-						"usage": command.usage
+		if self.client.is_ready():
+			data = {
+				"guilds": [guild.id for guild in self.client.guilds],
+				"users": [user.id for user in self.client.users],
+				"channels": [channel.id for channel in self.client.get_all_channels()],
+				"commands": {
+					cog_name: {
+						command.name: {
+							"description": command.description,
+							"usage": command.usage
+						}
+						for command in self.client.get_cog(cog_name).get_commands()
 					}
-					for command in self.client.get_cog(cog_name).get_commands()
-				}
-				for cog_name in self.client.cogs if cog_name in (
-					"Clans",
-					"Different",
-					"Economy",
-					"Games",
-					"Moderate",
-					"Settings",
-					"Utils",
-					"Works"
-				)
-			},
-		}
-		headers = {
-			"token": os.getenv("BOT_TOKEN")
-		}
-		await requests.post(url=self.api_url + "private/client", json=data, headers=headers)
+					for cog_name in self.client.cogs if cog_name in (
+						"Clans",
+						"Different",
+						"Economy",
+						"Games",
+						"Moderate",
+						"Settings",
+						"Utils",
+						"Works"
+					)
+				},
+			}
+			headers = {
+				"token": os.getenv("BOT_TOKEN")
+			}
+			await requests.post(url=self.api_url + "private/client", json=data, headers=headers)
 
 
 def setup(client):
