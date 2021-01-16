1530c108c0d07be2b06efd67cdc1b75d44cc4992
VadyChel <zyzel19@gmail.com>
2021-01-16 13:28:52 +0200

    Fix

--- cogs/different.py
+++ cogs/different.py
@@ -385,7 +385,7 @@ class Different(commands.Cog, name="Different"):
 					f"\n**Пользовательский статус:** {activity.emoji} {activity.name}"
 				)
 			else:
-				if activity.emoji in self.client.emojis():
+				if activity.emoji in self.client.emojis:
 					activity_info = f"\n**Пользовательский статус:** {activity.emoji} {activity.name}"
 				else:
 					activity_info = f"\n**Пользовательский статус:** {activity.name}"
