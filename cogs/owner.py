f84dc347494a86e7d52197c03f48e15132fee7eb
VadyChel <zyzel19@gmail.com>
2021-01-28 17:49:40 +0200

    Add command that edit dicts in database

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,4 +1,5 @@
 import traceback
+import json
 from discord.ext import commands
 
 
@@ -27,11 +28,41 @@ class Owner(commands.Cog, name="Owner"):
 	async def _rc(self, ctx, *, command: str):
 		command = self.client.get_command(command)
 		command.reset_cooldown(ctx)
+		await ctx.message.add_reaction("✅")
 
 	@commands.command()
 	@commands.is_owner()
-	async def _ed(self, ctx):
-		pass
+	async def _eda(self, ctx, action: str, column: str, *, key: str):
+		async with ctx.typing():
+			try:
+				if action == "update" or action == "u":
+					guilds = await self.client.database.execute(
+						f"SELECT guild_id, {column} FROM guilds"
+					)
+					for guild in guilds:
+						json_data = json.loads(guild[1])
+						json_data.update(json.loads(key))
+						await self.client.database.execute(
+							f"""UPDATE guilds SET {column} = %s WHERE guild_id = {guild[0]}""",
+							(json.dumps(json_data),)
+						)
+					await ctx.send(f"✅ `{column} has been edited(update)`")
+				elif action == "pop" or action == "p":
+					guilds = await self.client.database.execute(
+						f"SELECT guild_id, {column} FROM guilds"
+					)
+					for guild in guilds:
+						json_data = json.loads(guild[1])
+						json_data.pop(json.loads(key)["key"])
+						await self.client.database.execute(
+							f"""UPDATE guilds SET {column} = %s WHERE guild_id = {guild[0]}""",
+							(json.dumps(json_data),)
+						)
+					await ctx.send(f"✅ `{column} has been edited(pop)`")
+				else:
+					await ctx.send("❌ `Please select update or pop action`")
+			except Exception:
+				await ctx.send(f"```{traceback.format_exc()}```")
 
 
 def setup(client):
