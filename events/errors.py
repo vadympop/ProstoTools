8c95511e8dc50e7e412dfc7b7d439bd6799bbb9c
VadyChel <zyzel19@gmail.com>
2021-02-07 16:52:00 +0200

    Fixes

--- events/errors.py
+++ events/errors.py
@@ -33,7 +33,7 @@ class Errors(commands.Cog, name="Errors"):
 					description=f"**Кулдавн в команде еще не прошёл! Подождите {int(retry_after)} секунд**",
 					colour=discord.Color.green(),
 				)
-			elif retry_after > 60 and retry_after < 1800:
+			elif 60 < retry_after < 1800:
 				emb = discord.Embed(
 					title="Ошибка!",
 					description=f"**Кулдавн в команде еще не прошёл! Подождите {int(retry_after / 60)} минут**",
@@ -55,27 +55,13 @@ class Errors(commands.Cog, name="Errors"):
 				f"**Вы не указали аргумент. Укажити аргумент - {error.param.name} к указаной команде!**\n\n{ctx.command.help.format(Prefix=PREFIX)}\n[Документация](https://docs.prosto-tools.ml/)"
 				if ctx.command.help is not None
 				else "**Указан не правильный аргумент!\n[Документация](https://docs.prosto-tools.ml/)**",
-				False
+				bolf=False
 			)
 			await ctx.send(embed=emb)
 		elif isinstance(error, commands.errors.CommandNotFound):
 			pass
 		elif isinstance(error, commands.errors.NotOwner):
-			ctx.command.reset_cooldown(ctx)
-			try:
-				await ctx.message.add_reaction("❌")
-			except discord.errors.Forbidden:
-				pass
-			except discord.errors.HTTPException:
-				pass
-			emb = discord.Embed(
-				title="Ошибка!",
-				description="**Вы неявляетесь создателем бота! Эта команда только для создателей!**",
-				colour=discord.Color.green(),
-			)
-			emb.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-			emb.set_footer(text=self.FOOTER, icon_url=self.client.user.avatar_url)
-			await ctx.send(embed=emb)
+			pass
 		elif isinstance(error, commands.errors.MissingPermissions) or isinstance(error, commands.errors.CheckFailure):
 			ctx.command.reset_cooldown(ctx)
 			emb = await self.client.utils.create_error_embed(
@@ -89,16 +75,16 @@ class Errors(commands.Cog, name="Errors"):
 				f"**Указан не правильный аргумент!**\n\n{ctx.command.help.format(Prefix=PREFIX)}\n[Документация](https://docs.prosto-tools.ml/)"
 				if ctx.command.help is not None
 				else "**Указан не правильный аргумент!\n[Документация](https://docs.prosto-tools.ml/)**",
-				False
+				bold=False
 			)
 			await ctx.send(embed=emb)
 		elif isinstance(error, commands.errors.BotMissingPermissions):
-			owner = get(ctx.guild.members, id=ctx.guild.owner_id)
-			emb_err = await self.client.utils.create_error_embed(
+			ctx.command.reset_cooldown(ctx)
+			emb = await self.client.utils.create_error_embed(
 				ctx,
 				f"У бота отсутствуют права: {' '.join(error.missing_perms)}\nВыдайте их ему для полного функционирования бота"
 			)
-			await owner.send(embed=emb_err)
+			await ctx.send(embed=emb)
 		elif isinstance(error, commands.errors.MemberNotFound):
 			ctx.command.reset_cooldown(ctx)
 			emb = await self.client.utils.create_error_embed(ctx, "Указаный пользователь не найден!")
