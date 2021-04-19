f7b6b00c756cb734420109c17f8802a4ed30d91f
VadyChel <zyzel19@gmail.com>
2021-04-19 20:11:14 +0300

    Fix

--- cogs/economy/economy.py
+++ cogs/economy/economy.py
@@ -44,9 +44,9 @@ class Economy(BaseCog):
 		).exclude(exp__lte=0).order_by("-exp"):
 			member = ctx.guild.get_member(user.user_id)
 			if member is not None and not member.bot:
-				if num > 20*len(num_to_medal):
+				if num > 20*len(embeds):
 					embeds.append(emb)
-					emb = discord.Embed(title="Лидеры сервера", colour=discord.Color.green())
+					emb = discord.Embed(title=f"Лидеры сервера {20*len(embeds)}", colour=discord.Color.green())
 					emb.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
 					emb.set_footer(text=self.FOOTER, icon_url=self.client.user.avatar_url)
 
